410e68eb350689511441d623cc41df03bf5b1df7
thanhphathoang <125521127+HoangThanhPhat@users.noreply.github.com>
2024-07-22 17:15:20 +0700

    Update Layout.jsx

--- Frontend/src/components/Layout/Layout.jsx
+++ Frontend/src/components/Layout/Layout.jsx
@@ -1,36 +1,13 @@
-// import React, { Fragment } from 'react';
-// import { useLocation } from 'react-router-dom';
-// import Header from '../Header/Header';
-// import Footer from '../Footer/Footer';
-// import Routers from '../../routers/Routers';
-
-// const Layout = () => {
-//   const location = useLocation();
-//   const isLoginPage = location.pathname === '/login';
-
-//   return (
-//     <Fragment>
-//       {!isLoginPage && <Header />}
-//       <div>
-//         <Routers />
-//       </div>
-//       {!isLoginPage && <Footer />}
-//     </Fragment>
-//   );
-// };
-
-// export default Layout;
 import React, { Fragment } from 'react';
 import { useLocation, Route } from 'react-router-dom';
 import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
 import Routers from '../../routers/Routers';
 
 const Layout = () => {
   const location = useLocation();
 
   // Danh sách các route cần hiển thị Header và Footer
-  const routesWithHeaderFooter = ['/', '/home', '/about'];
+  const routesWithHeaderFooter = ['/', '/home', '/about','/admin/home'];
 
   // Kiểm tra xem route hiện tại có nằm trong danh sách hay không
   const shouldDisplayHeaderFooter = routesWithHeaderFooter.includes(location.pathname);
@@ -41,7 +18,6 @@ const Layout = () => {
       <div>
         <Routers />
       </div>
-      {shouldDisplayHeaderFooter && <Footer />}
     </Fragment>
   );
 };
